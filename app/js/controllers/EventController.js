47f33d12ca9051ecd1defee61a791b96e454338b
blibotics <blibotics@gmail.com>
2013-09-22 15:14:44 -0400

    Added an event object in the controller, used angular markup to call the object on the index page

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -1,3 +1,12 @@
 'use strict';
 
-eventsApp.controller('eventsController', function eventsController($scope) {})
+eventsApp.controller('eventsController', 
+	function eventsController($scope) {
+
+		$scope.event = {
+			name: 'Angular Boot Camp',
+			date: '12/25/2013',
+			time: '11:11 am'
+		}
+ 	}
+);
