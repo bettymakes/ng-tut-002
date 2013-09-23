69b65bc65505c860319229d54794acc32532dbe1
blibotics <blibotics@gmail.com>
2013-09-22 21:49:21 -0400

    Learning about ng-hide and ng-show

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -4,6 +4,7 @@ eventsApp.controller('eventsController',
 	function eventsController($scope) {
 
 		$scope.snippet='<span style="color:red">hi there!</span>';
+		$scope.booleanVal=false;
 		$scope.event = {
 			name: 'Angular Boot Camp',
 			date: '12/25/2013',
